55052d858a84aba6ba23d1838ef494e3ad0025f1
RickFqt <henriquefouquet@gmail.com>
2024-03-30 11:38:19 -0300

    chore(issue-02): Added Ortographic and Perspective Camera methods

--- src/core/camera.cpp
+++ src/core/camera.cpp
@@ -12,13 +12,30 @@ Ray Camera::generate_ray(int x, int y){
 }
 
 Ray PerspectiveCamera::generate_ray(int x, int y){
-    // TODO
-    return Ray{Vector3f{0,0,0},Vector3f{0,0,0}};
+
+    // Converts the (x,y) pixel coordinate to (i,j) coordinate in screen space
+    real_type i = screen_window[Borders_e::l] + (screen_window[Borders_e::r] - screen_window[Borders_e::l])*(x + 0.5)/film.m_full_resolution[0];
+    real_type j = screen_window[Borders_e::b] + (screen_window[Borders_e::t] - screen_window[Borders_e::b])*(y + 0.5)/film.m_full_resolution[1];
+
+    Point3f ray_origin = e;
+    Vector3f ray_direction = 1.F * w + i * u + j * v;
+    // We can assume that focal distance is 1
+
+
+    return Ray{ray_origin,ray_direction};
 }
 
 Ray OrthographicCamera::generate_ray(int x, int y){
-    // TODO
-    return Ray{Vector3f{0,0,0},Vector3f{0,0,0}};
+
+    // Converts the (x,y) pixel coordinate to (i,j) coordinate in screen space
+    real_type i = screen_window[Borders_e::l] + (screen_window[Borders_e::r] - screen_window[Borders_e::l])*(x + 0.5)/film.m_full_resolution[0];
+    real_type j = screen_window[Borders_e::b] + (screen_window[Borders_e::t] - screen_window[Borders_e::b])*(y + 0.5)/film.m_full_resolution[1];
+
+    Point3f ray_origin = e + i * u + j * v;
+    Vector3f ray_direction = w;
+
+
+    return Ray{ray_origin,ray_direction};
 }
 
 
