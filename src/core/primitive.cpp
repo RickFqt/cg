cd6ffb105d5c35b959e531c1ff4f9f4380d227cc
RickFqt <henriquefouquet@gmail.com>
2024-04-11 08:40:45 -0300

    feat(proj-04): Updated Primitve and initial GeometricPrimitive

---
+++ src/core/primitive.cpp
@@ -0,0 +1,15 @@
+#include "primitive.h"
+
+namespace rt3 {
+
+bool GeometricPrimitive::intersect_p( const Ray& r ) const{
+    // TODO
+    return true;
+}
+
+bool GeometricPrimitive::intersect( const Ray& r, Surfel *sf ) const{
+    // TODO
+    return true;
+}
+
+}
